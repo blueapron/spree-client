bbaecb6091cf423a1105f67b67851729405aefa9
Michael Collis <michael.collis@blueapron.com>
2016-06-02 08:16:32 -0400

    Revert timeout increase

--- lib/blue_apron/spree_client.rb
+++ lib/blue_apron/spree_client.rb
@@ -236,7 +236,7 @@ module BlueApron
       end
 
       def setup_timeouts(request)
-        request.options.timeout = timeout
+        # request.options.timeout = timeout
         request.options.open_timeout = timeout
       end
 
